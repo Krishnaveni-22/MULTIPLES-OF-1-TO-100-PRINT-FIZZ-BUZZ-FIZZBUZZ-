86134ca6719a8d40d80036ea4dc5befc2b79b775
Krishnaveni K <144761474+Krishnaveni-22@users.noreply.github.com>
2024-10-05 13:49:29 +0530

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+let num1 = prompt("Enter number 1")
+let num2 = prompt("Enter number 2")
+for(let i =num1;i<=num2;i++){
+    if(i%3==0 && i%5==0){
+        console.log("FizzBuzz");
+    }
+    else if(i%3==0){
+        console.log("Fizz");
+    }
+    else if(i%5==0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
